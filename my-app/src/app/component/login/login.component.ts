ffbdd8bbfe46bea408c32e6df4915d9cc7a39ba8
Vanderson Dias Mendes <109023638+VandersonMendes@users.noreply.github.com>
2024-05-03 13:18:05 -0300

    Revert "Login: validation form"

--- my-app/src/app/component/login/login.component.ts
+++ my-app/src/app/component/login/login.component.ts
@@ -1,56 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from 'src/app/service/theme.service';
-import { FormsModule } from '@angular/forms';
-import { BoundElementProperty } from '@angular/compiler';
 @Component({
   selector: 'app-login',
-  standalone: true,
+  standalone:true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [CommonModule, FormsModule]
+  imports :[CommonModule]
 })
-export class LoginComponent implements OnInit {
-  name: string = '';
-  email: string = '';
-  company: string = '';
-  errorForm: boolean = false;
-  errorMessage: string = '';
-  constructor(private themeService: ThemeService) {
-    const prefersTheme = localStorage.getItem('theme');
-    if (prefersTheme === 'dark') {
-      this.isToggleChangeTheme = true
-    }
-
-
+export class LoginComponent implements  OnInit {
+  constructor (private themeService: ThemeService){
+    
   }
   ngOnInit(): void {
-    const prefersTheme = localStorage.getItem('theme');
-    if (prefersTheme === 'dark') {
-      this.isToggleChangeTheme = true
+    // const prefersColorTheme =  window.matchMedia('(prefers-color-scheme: dark)');
+    // if(prefersColorTheme.matches){
+    //   localStorage.setItem('theme', 'dark')
+    //   this.isToggleChangeTheme = true;
+    // }else{
+    //   localStorage.setItem('theme', 'ligth')
+    //   this.isToggleChangeTheme = false;
+    // }
+  }
+  isToggleChangeTheme:  boolean = false;
+    isClickChangeTheme(){
+      this.isToggleChangeTheme = !this.isToggleChangeTheme
+      this.themeService.toggleDarkMode(!this.themeService.isDarkMode.value)
     }
-  }
-  isToggleChangeTheme: boolean = false;
-  isClickChangeTheme() {
-    this.isToggleChangeTheme = !this.isToggleChangeTheme
-    this.themeService.toggleDarkMode(!this.themeService.isDarkMode.value)
-  }
-  onSubmit(event: Event) {
-    event.preventDefault();
-    const regexEmailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-     console.log(regexEmailValidation.test(this.email))
-      if (this.name.length === 0 || this.email.length === 0 || this.company.length === 0) {
-      this.errorForm = true
-      this.errorMessage = 'Preencha todos os campos'
-    }else if(!regexEmailValidation.test(this.email)){
-      this.errorForm = true
-      this.errorMessage = 'Email invalido'
-      }else{
-      this.errorForm = false
-    }
-  }
-
-  onChangeForm(event: Event) {
-
-  }
 }
