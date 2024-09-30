71da9cf9f50cd718a4e85f07ee73aa5fd01b9b39
VandersonMendes <109023638+VandersonMendes@users.noreply.github.com>
2024-09-30 14:14:03 -0300

    fix: guard rotas home | feat: service ModalColaboradores

--- FRONT-END/my-app/src/app/home/component/painel-company/painel-company.component.ts
+++ FRONT-END/my-app/src/app/home/component/painel-company/painel-company.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from 'src/app/services/theme/theme.service';
@@ -7,15 +8,17 @@ import { ModalSidebarService } from '../../services/modalSidebar/modal-sidebar.s
 import { ContextService } from 'src/app/services/context/context.service';
 import { ApiService } from 'src/app/services/serviceApi/api.service';
 import { AutoLoginService } from 'src/app/services/auto-login/auto-login.service';
+import {Router} from '@angular/router'
+import { ModalColaboradoresService } from '../../services/modalColaboradores/modal-colaboradores.service';
 @Component({
-  selector: 'app-painel',
+  selector: 'app-painel-company',
+  templateUrl: './painel-company.component.html',
+  styleUrls: ['./painel-company.component.scss'],
   standalone: true,
-  templateUrl: './painel.component.html',
-  styleUrls: ['painel.component.scss'],
-  imports: [CommonModule, ModalComponent],
+  imports: [CommonModule, ModalComponent]
 })
-export class PainelComponent implements OnInit {
-  constructor(private themeService: ThemeService, private modalSidebar: ModalSidebarService, private context: ContextService, private apiService: ApiService, private autoLogionService: AutoLoginService) {
+export class PainelCompanyComponent {
+constructor(private themeService: ThemeService, private modalSidebar: ModalSidebarService, private context: ContextService, private apiService: ApiService, private autoLogionService: AutoLoginService, private router:Router, private modalColaboradoresService: ModalColaboradoresService) {
     themeService.isDarkMode$.subscribe((isDark: any) => {
       this.isToggleChangeTheme = isDark
     })
@@ -105,5 +108,9 @@ export class PainelComponent implements OnInit {
       })
     }
     )
+  }
+  navigateModalColaboradoresOpen(){
+    this.router.navigate(['/colaboradores'])
+    this.modalColaboradoresService.openModalColaboradores(true)
   }
 }
